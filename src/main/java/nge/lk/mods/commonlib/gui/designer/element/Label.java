5d811d21da98ca6acde594f9645b43b4bf91d925
LordKorea <lk97798@posteo.net>
2019-07-11 16:30:31 +0200

    added more shortcuts to the gui designer DSL to reduce boilerplate overhead
    (cherry picked from commit c3cbbd148aea91a7da95900151f11d060082231a)

--- src/main/java/nge/lk/mods/commonlib/gui/designer/element/Label.java
+++ src/main/java/nge/lk/mods/commonlib/gui/designer/element/Label.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import net.minecraft.client.Minecraft;
 import nge.lk.mods.commonlib.gui.designer.RenderContext;
 import nge.lk.mods.commonlib.gui.designer.RenderProperties;
+import nge.lk.mods.commonlib.gui.designer.util.Alignment;
 import nge.lk.mods.commonlib.gui.designer.util.Dimension;
 import nge.lk.mods.commonlib.gui.designer.util.RequestedSize;
 
@@ -22,6 +23,34 @@ public class Label extends BaseElement {
      * The color of this element (RGB).
      */
     private @Getter @Setter int color;
+
+    /**
+     * Creates a centered label that breaks the current group. The secondary alignment defaults to
+     * {@link nge.lk.mods.commonlib.gui.designer.util.Alignment#LEFT}.
+     *
+     * @param caption The label caption.
+     * @param color   The label color.
+     * @return The label.
+     */
+    public static Label centered(final String caption, final int color) {
+        return centered(caption, color, Alignment.LEFT);
+    }
+
+    /**
+     * Creates a centered label that breaks the current group.
+     *
+     * @param caption            The label caption.
+     * @param color              The label color.
+     * @param secondaryAlignment The secondary alignment.
+     * @return The label.
+     */
+    public static Label centered(final String caption, final int color, final Alignment secondaryAlignment) {
+        final Label label = new Label(RenderProperties.builder().centered().groupBreaking()
+                .secondaryAlignment(secondaryAlignment).build());
+        label.setText(caption, color);
+        label.pack();
+        return label;
+    }
 
     /**
      * Constructor.
